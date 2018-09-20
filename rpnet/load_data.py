fcc09795aa44d9adf6d61ebaa484bab3b6110efd
detectRecog <detectrecog@gmail.com>
2018-09-20 18:13:19 +0800

    add demo

--- rpnet/load_data.py
+++ rpnet/load_data.py
@@ -109,3 +109,28 @@ class ChaLocDataLoader(Dataset):
 
         # resizedImage = torch.from_numpy(resizedImage).float()
         return resizedImage, new_labels
+
+
+class demoTestDataLoader(Dataset):
+    def __init__(self, img_dir, imgSize, is_transform=None):
+        self.img_dir = img_dir
+        self.img_paths = []
+        for i in range(len(img_dir)):
+            self.img_paths += [el for el in paths.list_images(img_dir[i])]
+        # self.img_paths = os.listdir(img_dir)
+        # print self.img_paths
+        self.img_size = imgSize
+        self.is_transform = is_transform
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, index):
+        img_name = self.img_paths[index]
+        img = cv2.imread(img_name)
+        # img = img.astype('float32')
+        resizedImage = cv2.resize(img, self.img_size)
+        resizedImage = np.transpose(resizedImage, (2,0,1))
+        resizedImage = resizedImage.astype('float32')
+        resizedImage /= 255.0
+        return resizedImage, img_name
