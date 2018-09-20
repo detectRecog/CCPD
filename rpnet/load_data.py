149df6c71e6e77982d411d2b902356a4c5f61cf1
detectRecog <36831468+detectRecog@users.noreply.github.com>
2018-09-20 17:14:45 +0800

    Update load_data.py

--- rpnet/load_data.py
+++ rpnet/load_data.py
@@ -1,13 +1,7 @@
-import os
 from torch.utils.data import *
 from imutils import paths
 import cv2
-import torchvision
 import numpy as np
-import matplotlib.pyplot as plt
-import torch
-import scipy.misc as m
-from PIL import Image
 
 
 class labelFpsDataLoader(Dataset):
@@ -32,13 +26,14 @@ class labelFpsDataLoader(Dataset):
         resizedImage = np.transpose(resizedImage, (2,0,1))
         resizedImage = resizedImage.astype('float32')
         resizedImage /= 255.0
-        lbl = img_name.split('/')[-1].split('.')[0].split('-')[-3]
+        lbl = img_name.split('/')[-1].rsplit('.', 1)[0].split('-')[-3]
 
         iname = img_name.rsplit('/', 1)[-1].rsplit('.', 1)[0].split('-')
-        fps = [[int(eel) for eel in el.split('&')] for el in iname[3].split('_')]
-        leftUp, rightDown = [min([fps[el][0] for el in range(4)]), min([fps[el][1] for el in range(4)])], [
-            max([fps[el][0] for el in range(4)]), max([fps[el][1] for el in range(4)])]
-        ori_w, ori_h = [float(int(el)) for el in iname[1].split('_')]
+        # fps = [[int(eel) for eel in el.split('&')] for el in iname[3].split('_')]
+        # leftUp, rightDown = [min([fps[el][0] for el in range(4)]), min([fps[el][1] for el in range(4)])], [
+        #     max([fps[el][0] for el in range(4)]), max([fps[el][1] for el in range(4)])]
+        [leftUp, rightDown] = [[int(eel) for eel in el.split('&')] for el in iname[2].split('_')]
+        ori_w, ori_h = [float(int(el)) for el in [img.shape[1], img.shape[0]]]
         new_labels = [(leftUp[0] + rightDown[0]) / (2 * ori_w), (leftUp[1] + rightDown[1]) / (2 * ori_h),
                       (rightDown[0] - leftUp[0]) / ori_w, (rightDown[1] - leftUp[1]) / ori_h]
 
@@ -93,9 +88,15 @@ class ChaLocDataLoader(Dataset):
         resizedImage = np.reshape(resizedImage, (resizedImage.shape[2], resizedImage.shape[0], resizedImage.shape[1]))
 
         iname = img_name.rsplit('/', 1)[-1].rsplit('.', 1)[0].split('-')
-        fps = [[int(eel) for eel in el.split('&')] for el in iname[3].split('_')]
-        leftUp, rightDown = [min([fps[el][0] for el in range(4)]), min([fps[el][1] for el in range(4)])], [max([fps[el][0] for el in range(4)]), max([fps[el][1] for el in range(4)])]
-        ori_w, ori_h = 720, 1160
+        [leftUp, rightDown] = [[int(eel) for eel in el.split('&')] for el in iname[2].split('_')]
+
+        # tps = [[int(eel) for eel in el.split('&')] for el in iname[2].split('_')]
+        # for dot in tps:
+        #     cv2.circle(img, (int(dot[0]), int(dot[1])), 2, (0, 0, 255), 2)
+        # cv2.imwrite("/home/xubb/1_new.jpg", img)
+
+        ori_w, ori_h = float(img.shape[1]), float(img.shape[0])
+        assert img.shape[0] == 1160
         new_labels = [(leftUp[0] + rightDown[0])/(2*ori_w), (leftUp[1] + rightDown[1])/(2*ori_h), (rightDown[0]-leftUp[0])/ori_w, (rightDown[1]-leftUp[1])/ori_h]
 
         resizedImage = resizedImage.astype('float32')
@@ -108,4 +109,3 @@ class ChaLocDataLoader(Dataset):
 
         # resizedImage = torch.from_numpy(resizedImage).float()
         return resizedImage, new_labels
-
